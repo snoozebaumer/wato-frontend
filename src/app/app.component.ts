e976ddb702d58862a94183b1c99f8c9b595bce17
Samuel Nussbaumer <samuel.nussbaumer@stud.hslu.ch>
2024-02-12 18:33:26 +0100

    created challenge form wato#1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wato-frontend';
+  title = 'What are the Odds?';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateChallengeComponent } from './create-challenge/create-challenge.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateChallengeComponent
   ],
   imports: [
     BrowserModule,
