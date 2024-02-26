2adb2f98493e2eb0c917c0c190b44e1cd4ed7232
Samuel Nussbaumer <samuel.nussbaumer@stud.hslu.ch>
2024-02-26 23:29:15 +0100

    testing

--- src/app/components/challenge-creation/challenge-creation.component.spec.ts
+++ src/app/components/challenge-creation/challenge-creation.component.spec.ts
@@ -1,10 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import { ChallengeCreationComponent } from './challenge-creation.component';
 import {Pipe, PipeTransform} from "@angular/core";
+import {ChallengeService} from "../../services/challenge.service";
+import {of} from "rxjs";
+import {UserService} from "../../services/user.service";
+import {Router} from "@angular/router";
 
 @Pipe({name: 'translate'})
-class TranslatePipeMock implements PipeTransform {
+export class TranslatePipeMock implements PipeTransform {
   transform(value: string): string {
     switch(value) {
       case "GAME.CHALLENGE_TITLE":
@@ -28,7 +32,27 @@ describe('CreateChallengeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ChallengeCreationComponent, TranslatePipeMock]
+      declarations: [ChallengeCreationComponent, TranslatePipeMock],
+      providers: [{
+        provide: ChallengeService,
+        useValue: {
+          createChallenge: () => {
+            return {
+              subscribe: () => {of({id: "123"})}
+            }
+          }
+        }
+      },
+        {
+          provide: UserService,
+          useValue: {
+            user: {
+              id: "123",
+              name: "test"
+            }
+          }
+        }
+      ],
     })
     .compileComponents();
 
@@ -71,5 +95,52 @@ describe('CreateChallengeComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('button')?.textContent).toEqual('Challenge Button');
   });
+
+  it('should create challenge with correct inputs and navigate to share route', async () => {
+    const challengeService = TestBed.inject(ChallengeService);
+    const routerSpy = spyOn(TestBed.inject(Router), 'navigate').and.returnValue(Promise.resolve(true));
+
+    const fixture = TestBed.createComponent(ChallengeCreationComponent);
+    const component = fixture.componentInstance;
+    const createChallengeSpy = spyOn(challengeService, 'createChallenge').and.returnValue(of({ id: '123' }));
+
+    fixture.detectChanges();
+
+    component.create('chaufsch BIER?', 'Hans');
+
+    expect(createChallengeSpy).toHaveBeenCalledWith('chaufsch BIER?', 'Hans');
+
+    await fixture.whenStable();
+    expect(routerSpy).toHaveBeenCalledWith(['/share', '123']);
+  });
+
+  it('should set challenger input if userService.user is present', fakeAsync(() => {
+    const userService = TestBed.inject(UserService);
+    userService["_user"] = { id: "123", name: 'test' };
+
+    const fixture = TestBed.createComponent(ChallengeCreationComponent);
+    fixture.detectChanges();
+    tick();
+
+    const element = fixture.nativeElement as HTMLElement;
+    const challengerInput = element.querySelector('#name');
+
+    // @ts-ignore
+    expect(challengerInput["value"]).toEqual('test');
+  }));
+
+  it('should not disable challenger input if userService.user is not present', () => {
+    const userService = TestBed.inject(UserService);
+    userService["_user"] = undefined;
+
+    const fixture = TestBed.createComponent(ChallengeCreationComponent);
+    fixture.detectChanges();
+
+    const element = fixture.nativeElement as HTMLElement;
+    const challengerInput = element.querySelector('#name');
+
+    expect(challengerInput?.getAttribute('disabled')).toBeFalsy();
+  });
+
 
 });
--- src/app/components/challenge-detail/challenge-detail.component.spec.ts
+++ src/app/components/challenge-detail/challenge-detail.component.spec.ts
@@ -1,23 +1,200 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { ChallengeDetailComponent } from './challenge-detail.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ChallengeService } from '../../services/challenge.service';
+import { UserService } from '../../services/user.service';
+import { of } from 'rxjs';
+import { ChallengeStatus } from '../../models/challenge-status';
+import { FormsModule } from '@angular/forms';
+import { Challenge } from '../../models/challenge';
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({name: 'translate'})
+export class TranslatePipeMock implements PipeTransform {
+  transform(value: string): string {
+    switch(value) {
+      case "GAME.CHALLENGE_TITLE":
+        return "Challenge Title";
+      case "GAME.CHALLENGER":
+        return "Challenger";
+      case "GAME.CHALLENGER_PLACEHOLDER":
+        return "Challenger Placeholder";
+      case "GAME.CHALLENGE_BUTTON":
+        return "Challenge Button";
+      case "GAME.RANGE":
+        return "Range";
+      case "GAME.RANGE_PLACEHOLDER":
+        return "Range Placeholder";
+      case "GAME.NUMBER":
+        return "Number";
+      case "GAME.CONTINUE_BUTTON":
+        return "Continue";
+      case "GAME.ODDS":
+        return "Odds";
+      case "GAME.CHALLENGEE":
+        return "Challengee";
+    }
+    return "";
+  }
+}
 
 describe('ChallengeDetailComponent', () => {
   let component: ChallengeDetailComponent;
   let fixture: ComponentFixture<ChallengeDetailComponent>;
+  let mockActivatedRoute;
+  let mockRouter = jasmine.createSpyObj(['navigate']);
+  let mockChallengeService = jasmine.createSpyObj<ChallengeService>('ChallengeService', ['getChallenge', 'setRange', 'setGuess']);
+  let mockUserService = jasmine.createSpyObj('UserService', ['get']);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ChallengeDetailComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(() => {
+    mockActivatedRoute = {
+      snapshot: {
+        paramMap: {
+          get: jasmine.createSpy('get').and.returnValue('123')
+        }
+      }
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [ChallengeDetailComponent, TranslatePipeMock],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: Router, useValue: mockRouter },
+        { provide: ChallengeService, useValue: mockChallengeService },
+        { provide: UserService, useValue: mockUserService }
+      ],
+      imports: [FormsModule] // Add FormsModule for ngModel
+    });
+
     fixture = TestBed.createComponent(ChallengeDetailComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should retrieve challenge details on ngOnInit', fakeAsync(() => {
+    const mockChallenge = new Challenge("123", "holsch es bier?", "123", "Peter");
+    mockChallengeService.getChallenge.and.returnValue(of(mockChallenge));
+
+    fixture.detectChanges();
+    tick();
+
+    expect(component.challenge).toEqual(mockChallenge);
+  }));
+
+  it('should navigate to /share/:id if user is not involved in the game and state is GUESS_TO_BE_SET', fakeAsync(() => {
+    const mockChallenge =<Challenge>{
+      id: '123',
+      challenge: "hörsch uf Unit tests schriibe?",
+      challengeStatus: ChallengeStatus.GUESS_TO_BE_SET,
+      challengerId: '456',
+      challengerName: "Heinz",
+      challengeeId: '789'
+    };
+    mockUserService.user = { id: '999' };
+    mockChallengeService.getChallenge.and.returnValue(of(mockChallenge));
+
+    component.ngOnInit();
+    tick();
+
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/share', '123']);
+  }));
+
+  it('should navigate to /share/:id if user is not challenger and state is NEW', fakeAsync(() => {
+    const mockChallenge =<Challenge>{
+      id: '123',
+      challenge: "hörsch uf Unit tests schriibe?",
+      challengeStatus: ChallengeStatus.NEW,
+      challengerId: '456',
+      challengerName: "Heinz",
+    };
+    mockUserService.user = { id: '456' };
+    mockChallengeService.getChallenge.and.returnValue(of(mockChallenge));
+
+    component.ngOnInit();
+    tick();
+
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/share', '123']);
+  }));
+
+  it('should set range and update challenge status on setRange()', fakeAsync(() => {
+    component.formError = undefined;
+    const mockChallenge =<Challenge> {
+      id: '123',
+      challenge: "hörsch uf Unit tests schriibe?",
+      challengerId: '456',
+      challengeStatus: ChallengeStatus.NEW,
+      maxRange: 10,
+      challengerName: 'test',
+      challengeeName: 'Heiri'
+    };
+
+    mockChallengeService.getChallenge.and.returnValue(of(mockChallenge));
+    const returnChallenge = <Challenge>{...mockChallenge};
+    returnChallenge.challengeStatus = ChallengeStatus.GUESS_TO_BE_SET;
+    mockChallengeService.setRange.and.returnValue(of(returnChallenge));
+    tick();
+    fixture.detectChanges();
+    component.setRange();
+
+    expect(component.formError).toBeUndefined();
+    expect(mockChallengeService.setRange).toHaveBeenCalledWith('123', 10, 'Heiri');
+    expect(component.challenge.challengeStatus).toBe(ChallengeStatus.GUESS_TO_BE_SET);
+  }));
+
+  it('should set challengee guess and navigate to /share/:id on setChallengeeGuess()', fakeAsync(() => {
+    const mockChallenge =<Challenge> {
+      id: '123',
+      challenge: "hörsch uf Unit tests schriibe?",
+      challengerId: '456',
+      challengeStatus: ChallengeStatus.GUESS_TO_BE_SET,
+      maxRange: 10,
+      challengerName: 'test',
+      challengeeName: 'Heiri',
+      challengeeNumber: 7
+    };
+
+    mockChallengeService.getChallenge.and.returnValue(of(mockChallenge));
+    mockChallengeService.setGuess.and.returnValue(of(mockChallenge));
+
+    fixture.detectChanges();
+    component.setChallengeeGuess();
+    tick();
+
+
+    expect(mockChallengeService.setGuess).toHaveBeenCalledWith('123', 7, undefined);
+    expect(component.formError).toBeUndefined();
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/share', '123']);
+  }));
+
+
+  it('should finish game when challenger guess is set', fakeAsync(() => {
+    const mockChallenge =<Challenge> {
+      id: '123',
+      challenge: "hörsch uf Unit tests schriibe?",
+      challengerId: '456',
+      challengeStatus: ChallengeStatus.CHALLENGER_TO_MOVE,
+      maxRange: 10,
+      challengerName: 'test',
+      challengeeName: 'Heiri',
+      challengeeNumber: 7,
+      challengerNumber: 7
+    };
+
+    mockChallengeService.getChallenge.and.returnValue(of(mockChallenge));
+    const returnChallenge = <Challenge>{...mockChallenge};
+    returnChallenge.challengeStatus = ChallengeStatus.SUCCESS;
+    mockChallengeService.setGuess.and.returnValue(of(returnChallenge));
+
+    fixture.detectChanges();
+    component.setChallengerGuess();
+    tick();
+
+    expect(mockChallengeService.setGuess).toHaveBeenCalledWith('123', undefined, 7);
+    expect(component.formError).toBeUndefined();
+    expect(component.challenge).toEqual(returnChallenge);
+  }));
+
 });
